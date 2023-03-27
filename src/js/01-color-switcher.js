2c7cbb6e01a036b203b27e9390a1acf9de9f6566
Yana Lapach <y.lapach@gmail.com>
2023-03-27 13:19:59 +0200

    task1 - color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyChangeColor = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStop.setAttribute('disabled', '');
+
+btnStart.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
